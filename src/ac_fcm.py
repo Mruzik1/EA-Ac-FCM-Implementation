4b66edad50497fa195aa5696da3b6e2dfaef5c10
Mruzik1 <mimisha2014@gmail.com>
2023-04-22 17:05:16 +0200

    finished ac_fcm

--- src/ac_fcm.py
+++ src/ac_fcm.py
@@ -24,15 +24,46 @@ class AcFCM(BFCM):
         return U, V
 
     # applying absorbtive criteria
-    def absorbtive_criteria(self, inplace: bool = False) -> tuple:
+    def absorbtive_criteria(self) -> tuple:
         p, q = self.get_nearest_centroids()
         J_p = self.get_j_fcm(V=self.V[p])
         J_q = self.get_j_fcm(V=self.V[q])
         U, V = self.absorb_c(q, p) if J_p < J_q else self.absorb_c(p, q)
 
-        if inplace:
-            self.V = V
-            self.U = U
-            self.c -= 1
-
         return U, V
+
+    # check and update clusters for step 4
+    def update_clusters_4(self, new_U: np.ndarray, new_V: np.ndarray) -> bool:
+        if self.get_v_xb(new_U, new_V, self.c-1) < self.get_v_xb():
+            self.V = new_V
+            self.U = new_U
+            self.c -= 1
+            return True
+        return False
+
+    # check and update clusters for step 5
+    def update_clusters_5(self) -> bool:
+        super().run(logs_enabled=False)
+        old_U, old_V = self.U, self.V
+        self.U, self.V = self.absorbtive_criteria()
+        super().run(logs_enabled=False)
+
+        if self.get_v_xb(old_U, old_V) > self.get_v_xb(c=self.c-1):
+            self.c -= 1
+            return True
+        self.U, self.V = old_U, old_V
+        return False
+            
+    # running an algorithm
+    def run(self) -> np.ndarray:
+        # step 1 + 2
+        super().run(logs_enabled=False)
+        while self.c > 2:
+            # step 3 + 4
+            if self.update_clusters_4(*self.absorbtive_criteria()):
+                continue
+            # step 5
+            if self.update_clusters_5():
+                break
+        # step 6
+        return self.V
